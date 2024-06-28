0cd5137d6d7625be978e7e1e5d9b3fc8c26b9d7f
SanKRP2207 <sandeepkumarpal281@gmail.com>
2024-06-28 22:25:32 +0530

    done the .env file

--- db/config.js
+++ db/config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-const MONGO_URI = process.env.MONGO_URI.toString();
+const MONGO_URI = process.env.MONGO_URI ? process.env.MONGO_URI.toString() : '';
+
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
